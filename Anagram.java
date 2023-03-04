4fb89c585a67db1bc049e2a31e1cd4be46429e3c
akazzh-kumar <117301849+akazzh-kumar@users.noreply.github.com>
2023-03-04 18:20:03 +0530

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,14 +1,18 @@
 import java.util.Arrays;
+import java.util.Scanner;
 class Anagram{
 	public static void main(String[] args) {
-		String a ="Akashu";
-		String b ="Kaashu";
+		Scanner obj =new Scanner(System.in);
+		System.out.println("Enter the 1st String:");
+		String a = obj.nextLine();
+		System.out.println("Enter the 2nd String");
+		String b = obj.nextLine();
 		a=a.toLowerCase();
 		b=b.toLowerCase();
 		a=a.replaceAll(" ","");
 		b=b.replaceAll(" ","");
 		if(a.length()!=b.length()){
-			System.out.println("It is not Anagram");
+			System.out.println("This is not Equal. So It is not Anagram");
 		}
 		else 
 			{
@@ -17,7 +21,7 @@ class Anagram{
 				Arrays.sort(String1);
 				Arrays.sort(String2);
 				if(Arrays.equals(String1,String2)==true){
-					System.out.println("Anagram");
+					System.out.println("It is a Anagram");
 				}
 					else 
 						System.out.println("Not Anagram");
